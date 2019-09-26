97ee2326f49a23cdd9220af4a67bdee22ec2f0dc
lillianevans <54692228+lillianevans@users.noreply.github.com>
2019-09-26 01:06:32 -0400

    Add files via upload

--- exercise_4/js/scripts.js
+++ exercise_4/js/scripts.js
@@ -10,8 +10,6 @@ var d = new Date();
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "Sept", "October", "November", "December"];
 document.getElementById("getmonth").innerHTML = months[d.getMonth()];
 
-
-
 var d = new Date();
 document.getElementById("gethour").innerHTML = d.getHours();
                            
@@ -22,20 +20,11 @@ var d = new Date();
 document.getElementById("getsecond").innerHTML = d.getSeconds();
                            
 }
-function currentTime() {
-  var date = new Date(); /* creating object of Date class */
-  var hour = date.getHours();
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
-  var midday = "AM";
-  midday = (hour >= 12) ? "PM" : "AM"; /* assigning AM/PM */
-  hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* assigning hour in 12-hour format */
-  hour = updateTime(hour);
-  min = updateTime(min);
-  sec = updateTime(sec);
-  document.getElementById("date").innerText = hour + " : " + min + " : " + sec + " " + midday; /* adding time to the div */
-    var t = setTimeout(currentTime, 1000); /* setting timer */
-}
+function formatAMPM(date) {var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+minutes = minutes < 10 ? '0'+minutes : minutes;}
+
 
 function updateTime(k) { /* appending 0 before time elements if less than 10 */
   if (k < 10) {
