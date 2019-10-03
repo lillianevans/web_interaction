1fc8109892d58386b82f3b303ffe1f6153467745
lillianevans <54692228+lillianevans@users.noreply.github.com>
2019-10-03 15:59:54 -0400

    Add files via upload

---
+++ exercise_5/js/scripts.js
@@ -0,0 +1,38 @@
+//change the font family of clock
+function fontFunction(){
+    var font = document.getElementById("myFonts").value;
+    logo.style.fontFamily = font;
+}
+//bold
+function boldFunction(){
+    var checkBox = document.getElementById("boldCheck");
+    console.log(checkBox.checked)
+    if (checkBox.checked == true ){
+        logo.style.fontWeight = "bold";
+    }
+    else{
+        logo.style.fontWeight = 'inherit';
+    }
+}
+    
+function italicFunction(){
+    var checkBox = document.getElementById("italicCheck");
+    console.log(checkBox.checked)
+    if (checkBox.checked == true ){
+        logo.style.fontStyle = "italic";
+    }
+    else{
+        logo.style.fontStyle = 'inherit';
+    }
+}
+
+//size
+function sizeFunction(){
+    var checkSize = document.getElementById("size").value;
+    console.log(checkSize)
+    logo.style.fontSize = checkSize + "px";
+}
+
+function myFunction() {
+  document.body.style.backgroundImage= "url(images/background3.png)";
+}
