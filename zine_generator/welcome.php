0503afe92bed62bbc748456d9940c0ade792c3ac
lillianevans <54692228+lillianevans@users.noreply.github.com>
2019-11-07 12:18:16 -0500

    Add files via upload

---
+++ zine_generator/welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Title: <?php echo $_POST["title"]; ?><br>
+Name(s) of contributor(s): <?php echo $_GET["contributors"]; ?>
+Body Text: <?php echo $_POST["text"]; ?>
+
+</body>
+</html>
